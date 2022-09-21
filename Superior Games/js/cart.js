fc63f1ca09d3a67eac5dd29afd13dfbb1e3f9c6f
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-09-21 22:52:38 +0500

    database update

--- Superior Games/js/cart.js
+++ Superior Games/js/cart.js
@@ -1,5 +1,3 @@
-
-
 //   <tr>
 //     <td data-th="Product">
 //       <div class="row">
@@ -30,40 +28,70 @@
 //       </button>
 //     </td>
 //   </tr>
-let addtoCart=()=>{
+let addtoCart = () => {
+  let setHead = document.getElementById("setHead");
+  let setPrice = document.getElementById("setPrice");
+  let setImg = document.getElementById("setImg");
+  let quantityProduct = document.getElementById("quantity-product");
+var key = firebase.database().ref("Cart").push().key;
+  var cardData = {
+    heading: setHead.innerHTML,
+    price: setPrice.innerHTML,
+    img: setImg.src,
+    quantity: quantityProduct.value,
+    key: key,
+  };
+
+  firebase.database().ref("Cart").child(key).set(cardData);
+  
  
+  //   firebase
+  //     .database()
+  //     .ref("Cart")
+  //     .child(key, function (data) {
+  //       if (data.val().heading === setHead.innerHTML) {
+  //         console.log(data.val().heading, "<<<yes>>>", setHead.innerHTML);
+  //       } else {
+  //         console.log(data.val().heading, "<<<no>>>", setHead.innerHTML);
+  //     }
+  //     console.log(data.val().heading, "<<<data>>>", setHead.innerHTML);
+  //     });
+};
 
+// firebase
+//   .database()
+//   .ref("Users")
+//   .on("child_added", function (data) {
+//     var li = document.createElement("li");
+//     li.setAttribute("class", "list");
+//     var input = document.createElement("input");
+//     input.setAttribute("class", "enters");
+//     input.setAttribute("id", "inpt");
+//     input.setAttribute("value", data.val().value);
+//     input.setAttribute("readonly", "readonly");
+//     li.appendChild(input);
 
-    let setCart = document.getElementById("setCart");
+//     var editbtn = document.createElement("button");
+//     editbtn.setAttribute("id", data.val().key);
+//     editbtn.setAttribute("onclick", "editval(this)");
+//     editbtn.setAttribute("class", "btns");
+//     var edittext = document.createTextNode("Edit");
+//     editbtn.appendChild(edittext);
+//     li.appendChild(editbtn);
+//     var dellbtn = document.createElement("button");
+//     dellbtn.setAttribute("id", data.val().key);
+//     dellbtn.setAttribute("onclick", "dellval(this)");
+//     dellbtn.setAttribute("class", "btns");
+//     var delltext = document.createTextNode("Delete");
+//     dellbtn.appendChild(delltext);
+//     li.appendChild(dellbtn);
 
+//     var record = document.getElementById("record");
+//     record.appendChild(li);
+//     console.log(data.val().value);
+//   });
 
-
-}
-
-var li = document.createElement("li");
-li.setAttribute("class", "list");
-var input = document.createElement("input");
-input.setAttribute("class", "enters");
-input.setAttribute("id", "inpt");
-input.setAttribute("value", data.val().value);
-input.setAttribute("readonly", "readonly");
-li.appendChild(input);
-
-var editbtn = document.createElement("button");
-editbtn.setAttribute("id", data.val().key);
-editbtn.setAttribute("onclick", "editval(this)");
-editbtn.setAttribute("class", "btns");
-var edittext = document.createTextNode("Edit");
-editbtn.appendChild(edittext);
-li.appendChild(editbtn);
-var dellbtn = document.createElement("button");
-dellbtn.setAttribute("id", data.val().key);
-dellbtn.setAttribute("onclick", "dellval(this)");
-dellbtn.setAttribute("class", "btns");
-var delltext = document.createTextNode("Delete");
-dellbtn.appendChild(delltext);
-li.appendChild(dellbtn);
-
-var record = document.getElementById("record");
-record.appendChild(li);
-console.log(data.val().value);
+// function dellval(o) {
+//   firebase.database().ref("Users").child(o.id).remove();
+//   o.parentNode.remove();
+// }
