4fcc6207d394e6bb9018b6751ee63ce6d5714255
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-09-22 13:39:09 +0500

    database update

--- Superior Games/js/cart.js
+++ Superior Games/js/cart.js
@@ -1,11 +1,12 @@
-
 let addtoCart = () => {
   let setHead = document.getElementById("setHead");
   let setPrice = document.getElementById("setPrice");
+  let addedAlert = document.getElementById("added-alert");
+
   let setImg = document.getElementById("setImg");
   let cartAddBtn = document.getElementById("cartAddBtn");
   let quantityProduct = document.getElementById("quantity-product");
-var key = firebase.database().ref("Cart").push().key;
+  var key = firebase.database().ref("Cart").push().key;
   var cardData = {
     heading: setHead.innerHTML,
     price: setPrice.innerHTML,
@@ -14,14 +15,15 @@ var key = firebase.database().ref("Cart").push().key;
     key: key,
   };
 
+
+  if(setHead.innerHTML != ""){
   firebase.database().ref("Cart").child(key).set(cardData);
-  
-  cartAddBtn.setAttribute("disabled", true)
+  cartAddBtn.innerHTML="Added to Cart Successfully"
+  }else{
+
+    cartAddBtn.innerHTML="No Product Selected"
+  }
+
+  cartAddBtn.setAttribute("disabled", true);
 };
 
-
-
-// function dellval(o) {
-//   firebase.database().ref("Users").child(o.id).remove();
-//   o.parentNode.remove();
-// }
--- Superior Games/js/data.js
+++ Superior Games/js/data.js
@@ -2,17 +2,15 @@ firebase
   .database()
   .ref("Cart")
   .on("child_added", function (data) {
-    console.log(data.val().heading);
-    console.log(data.val().img);
-    console.log(data.val().key);
-    console.log(data.val().price);
+     
     let tr = document.createElement("tr");
+    let br = document.createElement("br");
     // First td
     let td = document.createElement("td");
     let div = document.createElement("div");
     let firstInnerdiv = document.createElement("div");
     let img = document.createElement("img");
-    
+
     let secondInnerdiv = document.createElement("div");
     let title = document.createElement("h4");
     // First td
@@ -24,7 +22,7 @@ firebase
 
     let btn = document.createElement("button");
     let icon = document.createElement("i");
-    
+
     // Second td
     // Second td
     pricetd.setAttribute("class", "data-tbl");
@@ -32,24 +30,25 @@ firebase
     subTotal.setAttribute("class", "data-tbl text-center");
     deleteBtn.setAttribute("class", "actions");
 
-
     btn.setAttribute("class", "btn btn-danger btn-sm");
     icon.setAttribute("class", "fa fa-trash-o");
-
 
     // Second td
     // First td
     title.setAttribute("class", "nomargin");
     secondInnerdiv.setAttribute("class", "col-sm-10");
+    deleteBtn.setAttribute("id", data.val().key);
+
+    deleteBtn.setAttribute("onclick", "dellval(this)");
     img.setAttribute("src", data.val().img);
     title.innerHTML = data.val().heading;
-    pricetd.innerHTML = data.val().price;
+    pricetd.innerHTML = "$" + data.val().price;
     quantitytd.innerHTML = data.val().quantity;
+    subTotal.innerHTML = "$" + data.val().price * data.val().quantity;
     img.setAttribute("class", "img-responsive");
     firstInnerdiv.setAttribute("class", "col-sm-2 hidden-xs");
     div.setAttribute("class", "row");
     // First td
-
 
     div.appendChild(firstInnerdiv);
     div.appendChild(secondInnerdiv);
@@ -63,53 +62,22 @@ firebase
     tr.appendChild(quantitytd);
     tr.appendChild(subTotal);
     tr.appendChild(deleteBtn);
-    // var editbtn = document.createElement("button");
-    // editbtn.setAttribute("id", data.val().key);
-    // editbtn.setAttribute("onclick", "editval(this)");
-    // editbtn.setAttribute("class", "btns");
-    // var edittext = document.createTextNode("Edit");
-    // editbtn.appendChild(edittext);
-    // li.appendChild(editbtn);
-    // var dellbtn = document.createElement("button");
-    // dellbtn.setAttribute("id", data.val().key);
-    // dellbtn.setAttribute("onclick", "dellval(this)");
-    // dellbtn.setAttribute("class", "btns");
-    // var delltext = document.createTextNode("Delete");
-    // dellbtn.appendChild(delltext);
-    // li.appendChild(dellbtn);
 
-    var setCart = document.getElementById("setCart");
-    setCart.appendChild(tr);
-    // console.log(data.val().value);
+    var setCart = document.getElementById("setCart"); 
+    // setCart.appendChild(br2); 
+    setCart.appendChild(tr); 
+    // setCart.appendChild(br); 
+  
   });
 
-//   <tr>
-//     <td data-th="Product">
-//       <div class="row">
-//         <div class="col-sm-2 hidden-xs">
-//           <img
-//             src="http://placehold.it/100x100"
-//             alt="..."
-//             class="img-responsive"
-//           />
-//         </div>
-//         <div class="col-sm-10">
-//           <h4 class="nomargin">Product 1</h4>
-//         </div>
-//       </div>
-//     </td>
-//     <td colspan="1" class="data-tbl" data-th="Price">
-//       $1.99
-//     </td>
-//     <td colspan="0" class="data-tbl dt-t" data-th="Quantity">
-//       1
-//     </td>
-//     <td colspan="0" class="data-tbl text-center" data-th="Subtotal">
-//       1.99
-//     </td>
-//     <td class="actions" data-th="">
-//       <button class="btn btn-danger btn-sm">
-//         <i class="fa fa-trash-o"></i>
-//       </button>
-//     </td>
-//   </tr>
+
+function dellval(o) {
+//   console.log(o.id);
+  firebase.database().ref("Cart").child(o.id).remove();
+    o.parentNode.remove();
+}
+
+function deleteBtn() {
+  firebase.database().ref("Cart").remove();
+  setCart.innerHTML = "";
+}
